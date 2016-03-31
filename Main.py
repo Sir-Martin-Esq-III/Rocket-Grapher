b6c152a4bbfe6c091c82de5f7f137dc915212150
Thomas Martin <ThomasMartin98@hotmail.com>
2016-03-31 23:52:46 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 """TODO:
 - Creater an event handler - May not need to if the only event is a mouse click and enter on the entries 
 - Create the final GUI layout for the program
--- Fonts ect
 -- Have different lables for "Max height"(Example) and the "value" (Although if they are going to look the same I don't think it will )
-- Import and configure MatplotLib (And maybe numpy)
+- Import and configure MatplotLib 
 - Import Threading? Would make it quicker and could do the calcs on one thred and draw the graph on the other
 - Change the amount of sig figs that the values show? Perhaps get the user to give it...
     
@@ -13,6 +12,12 @@
 """
 from tkinter import *
 from math import *
+
+#Fonts
+MainlableFont=("TkDefaultFont",15,"bold")
+infoLableFont=("TkDefaultFont",10,"bold")
+launchFont=("TkDefaultFont",15,"bold")
+
 #Does all of the calculations 
 def maths(angle,velocity):
     gravity=9.81
@@ -31,11 +36,11 @@ def ErrorHandler(value):
     hDisplacement.set("")
     Time.set("")
     if value==1:#Value Error
-        errorText.config(text="Value Error: Please enter a number")
+        errorText.config(text=" Value Error: Please enter a number")
     if value==2:#Angle >360
-        errorText.config(text="Angle Value Error: An angle must follow this rule:\n 0<= ANGLE <=360")
+        errorText.config(text=" Angle Value Error: An angle must follow this rule:\n 0<= ANGLE <=360")
     if value==3:#Velocity<0
-        errorText.config(text="Velocity Value Error: You can't have a negative velocity.")
+        errorText.config(text=" Velocity Value Error: You can't have a negative velocity.")
     
 #Launch function- This is where the calculations will take place as well as makeing the user chose a number.
 def launch(event):
@@ -88,39 +93,39 @@ inputPos.place(x=width*0.66,y=0)
 #Angle information- Creates a lable and an entry box (Which can be cleared) as well as declares the variable for the value of launch velocity
 angleValue=StringVar()
 angleValue.set('Use values > 0')    
-lable_LaunchAngle=Label(inputPos, text="Launch Angle: ",fg ="#0066ff",bg ="White")
+lable_LaunchAngle=Label(inputPos, text="Launch Angle: ",fg ="#0066ff",bg ="White",font=MainlableFont)
 lable_LaunchAngle.place(x=0,y=0)
 entry_LaunchAngle=Entry(inputPos, textvariable=angleValue,fg ="#0066ff",bg ="#cce6ff",relief=FLAT)
-entry_LaunchAngle.place(x=150,y=0)
+entry_LaunchAngle.place(x=150,y=7)
 entry_LaunchAngle.bind("<1>",clear1)
 
 #Launch velocity information- Creates a lable and an entry box (Which can be cleared) as well as declares the variable for the value of launch velocity
 LaunchVel=StringVar()
 LaunchVel.set('Use values > 0')
-lable_LaunchVel=Label(inputPos, text="Resulatant Velocity: ",fg ="#0066ff",bg ="White")
+lable_LaunchVel=Label(inputPos, text="Resulatant Velocity: ",fg ="#0066ff",bg ="White",font=MainlableFont)
 lable_LaunchVel.place(x=2,y=50)
 entry_LaunchVel=Entry(inputPos, textvariable=LaunchVel,fg ="#0066ff",bg ="#cce6ff",relief=FLAT)
-entry_LaunchVel.place(x=150,y=50)
+entry_LaunchVel.place(x=200,y=57)
 entry_LaunchVel.bind("<FocusIn>",clear2)
 7
 #Launch button- Creates a button which "launches" the rocket (goes to the launch Procedure)
-launchButton=Button(inputPos,text="LAUNCH",fg ="#0066ff",relief=FLAT,bg="#cce6ff")
-launchButton.place(x=0,y=100)
+launchButton=Button(inputPos,text="LAUNCH",fg ="#0066ff",relief=FLAT,bg="#cce6ff",font=MainlableFont)
+launchButton.place(x=100,y=100)
 launchButton.bind("<1>",launch)
 
 #Errors- Shows any errors to the user.
 errorText=Label(inputPos, text="",fg ="Red",bg ="White")
-errorText.place(x=0,y=150)
+errorText.place(x=0,y=height-30)
 
-#Ouput Lables- Shows the maximum height, the horizontal displacement and Time in the air
+#Ouput Lables- Shows the maximum height of the projectile and the horizontal displacement
 vDisplacement=StringVar()
 hDisplacement=StringVar()
 Time=StringVar()
-Lable_vDisplacement=Label(inputPos, textvariable=vDisplacement,fg ="Red",bg ="White")
+Lable_vDisplacement=Label(inputPos, textvariable=vDisplacement,fg ="Red",bg ="White",font=infoLableFont)
 Lable_vDisplacement.place(x=0,y=150)
-Lable_hDisplacement=Label(inputPos, textvariable=hDisplacement,fg ="Red",bg ="White")
+Lable_hDisplacement=Label(inputPos, textvariable=hDisplacement,fg ="Red",bg ="White",font=infoLableFont)
 Lable_hDisplacement.place(x=0,y=200)
-Lable_Time=Label(inputPos, textvariable=Time,fg ="Red",bg ="White")
+Lable_Time=Label(inputPos, textvariable=Time,fg ="Red",bg ="White",font=infoLableFont)
 Lable_Time.place(x=0,y=250)
 
 #Main loop
