368ffcb7e180424e041649cb1a73eed8ccaf27a6
Thomas Martin <ThomasMartin98@hotmail.com>
2016-04-03 19:49:09 +0100

    Update Main.py
    Added a graph to the window and changed the labels position on the window.

--- Main.py
+++ Main.py
@@ -2,21 +2,28 @@
 - Creater an event handler - May not need to if the only event is a mouse click and enter on the entries 
 - Create the final GUI layout for the program
 -- Have different lables for "Max height"(Example) and the "value" (Although if they are going to look the same I don't think it will )
-- Import and configure MatplotLib 
-- Import Threading? Would make it quicker and could do the calcs on one thred and draw the graph on the other
+- Create the final look of the graph as well as find a way of updateing the graph when the user enters new data
+- Import Threading? Would make it quicker and could do the calcs on one thread and draw the graph on the other
 - Change the amount of sig figs that the values show? Perhaps get the user to give it...
     
     Current Bugs:
 - Functions
--- Can't find a reliable way of finding the widget an event has came from
+-- Can't find a reliable way of finding the widget an event has came from (For a basic event handler)
 """
 from tkinter import *
 from math import *
+#Matplotlib and its backend and other stuff.
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
-#Fonts
+#Fonts/Styles
 MainlableFont=("TkDefaultFont",15,"bold")
 infoLableFont=("TkDefaultFont",10,"bold")
 launchFont=("TkDefaultFont",15,"bold")
+plt.style.use(['ggplot'])
 
 #Does all of the calculations 
 def maths(angle,velocity):
@@ -94,23 +101,23 @@ inputPos.place(x=width*0.66,y=0)
 angleValue=StringVar()
 angleValue.set('Use values > 0')    
 lable_LaunchAngle=Label(inputPos, text="Launch Angle: ",fg ="#0066ff",bg ="White",font=MainlableFont)
-lable_LaunchAngle.place(x=0,y=0)
+lable_LaunchAngle.place(x=0,y=50)
 entry_LaunchAngle=Entry(inputPos, textvariable=angleValue,fg ="#0066ff",bg ="#cce6ff",relief=FLAT)
-entry_LaunchAngle.place(x=150,y=7)
+entry_LaunchAngle.place(x=150,y=57)
 entry_LaunchAngle.bind("<1>",clear1)
 
 #Launch velocity information- Creates a lable and an entry box (Which can be cleared) as well as declares the variable for the value of launch velocity
 LaunchVel=StringVar()
 LaunchVel.set('Use values > 0')
-lable_LaunchVel=Label(inputPos, text="Resulatant Velocity: ",fg ="#0066ff",bg ="White",font=MainlableFont)
-lable_LaunchVel.place(x=2,y=50)
+lable_LaunchVel=Label(inputPos, text="Resulatant Velocity: ",fg ="#0066ff",bg ="White",font=MainlableFont)     
+lable_LaunchVel.place(x=2,y=100)
 entry_LaunchVel=Entry(inputPos, textvariable=LaunchVel,fg ="#0066ff",bg ="#cce6ff",relief=FLAT)
-entry_LaunchVel.place(x=200,y=57)
+entry_LaunchVel.place(x=200,y=107)
 entry_LaunchVel.bind("<FocusIn>",clear2)
-7
+
 #Launch button- Creates a button which "launches" the rocket (goes to the launch Procedure)
 launchButton=Button(inputPos,text="LAUNCH",fg ="#0066ff",relief=FLAT,bg="#cce6ff",font=MainlableFont)
-launchButton.place(x=100,y=100)
+launchButton.place(x=100,y=150)
 launchButton.bind("<1>",launch)
 
 #Errors- Shows any errors to the user.
@@ -122,11 +129,21 @@ vDisplacement=StringVar()
 hDisplacement=StringVar()
 Time=StringVar()
 Lable_vDisplacement=Label(inputPos, textvariable=vDisplacement,fg ="Red",bg ="White",font=infoLableFont)
-Lable_vDisplacement.place(x=0,y=150)
+Lable_vDisplacement.place(x=0,y=200)
 Lable_hDisplacement=Label(inputPos, textvariable=hDisplacement,fg ="Red",bg ="White",font=infoLableFont)
-Lable_hDisplacement.place(x=0,y=200)
+Lable_hDisplacement.place(x=0,y=250)
 Lable_Time=Label(inputPos, textvariable=Time,fg ="Red",bg ="White",font=infoLableFont)
-Lable_Time.place(x=0,y=250)
+Lable_Time.place(x=0,y=300)
+
+#Graph -Note, this is to test what the graph will look like in the window and the data is hard coded to the graph. This won't be the case in the actual program.
+f = Figure(figsize=(6.35,5.5), dpi=100) 
+a = f.add_subplot(111)
+a.plot([1,2,3,4,5],[32,1,2,3,4])
+plt.ylabel('some numbers')
+plt.xlabel('some numbers')
+canvas = FigureCanvasTkAgg(f, graphPos)
+canvas.show()
+canvas.get_tk_widget().place(x=0,y=0)
 
 #Main loop
 root.mainloop()
