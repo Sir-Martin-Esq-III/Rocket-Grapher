d21a2ee8e13dc69c866bf00e13284e34e81b0498
Thomas Martin <ThomasMartin98@hotmail.com>
2016-06-11 18:10:06 +0100

    Update CurrentDevBuild.py

--- CurrentDevBuild.py
+++ CurrentDevBuild.py
@@ -6,11 +6,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 from matplotlib import style
 from matplotlib import animation
-style.use('ggplot')
-
-#Fonts/Styles
-mainFont=("TkDefaultFont",10,"bold")
-titleFont=("TkDefaultFont",12,"bold")
 
 
 sideFrames=[None,None]
@@ -18,15 +13,18 @@ r0StagesList=list()#List of the stages for Rocket0
 r1StagesList=list() #List of the stages for Rocket1
 
 root = Tk()
+#Gets the height and width of the monitor
 width=root.winfo_screenwidth()/2
 height=root.winfo_screenheight()/2
 root.title("Rocket Science 9000")
 root.geometry("%dx%d+%d+%d" %(width,height,width/2,height/2))
-root.config(background = 'white')
+#root.config(background = 'white')
 root.resizable(False,False)
 
 #StageFrame
 class stageFrame:
+    mainFont=("TkDefaultFont",10,"bold")
+    titleFont=("TkDefaultFont",12,"bold")
     #Current stage numbers for each rocket
     r0currentStageNumber=0
     r1currentStageNumber=0
@@ -70,22 +68,25 @@ class stageFrame:
 
         #Saves the current stage
         def saveStage(event):
-            if sideNumber==0:
-                print("Saving stage%i from input field %i"%(self.r0currentStageNumber,sideNumber))
-                r0StagesList[self.r0currentStageNumber].mass=float(Entry_Mass.get())
-                r0StagesList[self.r0currentStageNumber].angle=float(Entry_Angle.get())
-                r0StagesList[self.r0currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
-                r0StagesList[self.r0currentStageNumber].thrust=float(Entry_Thrust.get())
-                r0StagesList[self.r0currentStageNumber].burnTime=float(Entry_burnTime.get())
-                r0StagesList[self.r0currentStageNumber].stageColor=str(Entry_stageColor.get())
-            else:
-                print("Saving stage%i from input field %i"%(self.r1currentStageNumber,sideNumber))
-                r1StagesList[self.r1currentStageNumber].mass=float(Entry_Mass.get())
-                r1StagesList[self.r1currentStageNumber].angle=float(Entry_Angle.get())
-                r1StagesList[self.r1currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
-                r1StagesList[self.r1currentStageNumber].thrust=float(Entry_Thrust.get())
-                r1StagesList[self.r1currentStageNumber].burnTime=float(Entry_burnTime.get())
-                r1StagesList[self.r1currentStageNumber].stageColor=str(Entry_stageColor.get())
+            try:
+                if sideNumber==0:
+                    print("Saving stage%i from input field %i"%(self.r0currentStageNumber,sideNumber))
+                    r0StagesList[self.r0currentStageNumber].mass=float(Entry_Mass.get())
+                    r0StagesList[self.r0currentStageNumber].angle=float(Entry_Angle.get())
+                    r0StagesList[self.r0currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
+                    r0StagesList[self.r0currentStageNumber].thrust=float(Entry_Thrust.get())
+                    r0StagesList[self.r0currentStageNumber].burnTime=float(Entry_burnTime.get())
+                    r0StagesList[self.r0currentStageNumber].stageColor=str(Entry_stageColor.get())
+                else:
+                    print("Saving stage%i from input field %i"%(self.r1currentStageNumber,sideNumber))
+                    r1StagesList[self.r1currentStageNumber].mass=float(Entry_Mass.get())
+                    r1StagesList[self.r1currentStageNumber].angle=float(Entry_Angle.get())
+                    r1StagesList[self.r1currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
+                    r1StagesList[self.r1currentStageNumber].thrust=float(Entry_Thrust.get())
+                    r1StagesList[self.r1currentStageNumber].burnTime=float(Entry_burnTime.get())
+                    r1StagesList[self.r1currentStageNumber].stageColor=str(Entry_stageColor.get())
+            except ValueError:
+                print("An error occoured causing the stage not to save")
         #Changes the current stage
         def changeStageState(event,option):
              #Add a stage. Does this by increaseing the stageNumber, createing a class instance of "Stages" and then appending that instance to and array
@@ -143,53 +144,53 @@ class stageFrame:
         #Place the new frame
         self.newframe.place(x=x,y=0)
         """---BEGIN UI PLACEMENT---"""
-        lable_rocketValue=Label(self.newframe,text="Rocket "+str(sideNumber),fg ="#8c8c8c",bg ="White",font=titleFont)
+        lable_rocketValue=Label(self.newframe,text="Rocket "+str(sideNumber),fg ="#8c8c8c",bg ="White",font=self.titleFont)
         lable_rocketValue.place(x=width/4,y=0)
 
         #Stage number
         stageValue=StringVar()
         stageValue.set("Stage 0")    
-        lable_stageValue=Label(self.newframe, textvariable=stageValue,fg ="#8c8c8c",bg ="White",font=mainFont)
+        lable_stageValue=Label(self.newframe, textvariable=stageValue,fg ="#8c8c8c",bg ="White",font=self.mainFont)
         lable_stageValue.place(x=width/4,y=20)
         
         #Mass widgets(Lable&Entry)
         mass=DoubleVar()
-        lable_Mass=Label(self.newframe, text="Mass(KG)",fg ="#8c8c8c",bg ="White",font=mainFont)   
+        lable_Mass=Label(self.newframe, text="Mass(KG)",fg ="#8c8c8c",bg ="White",font=self.mainFont)   
         lable_Mass.place(x=0,y=40)  
         Entry_Mass=Entry(self.newframe,textvariable=mass, fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_Mass.place(x=0,y=60)
         
         #Angle widgets(Lable&Entry)
         angle=DoubleVar()
-        lable_Angle=Label(self.newframe, text="Angle(°)",fg ="#8c8c8c",bg ="White",font=mainFont)     
+        lable_Angle=Label(self.newframe, text="Angle(°)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
         lable_Angle.place(x=0,y=80)
         Entry_Angle=Entry(self.newframe, textvariable=angle,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_Angle.place(x=0,y=100)
         
         #Fuel widgets(Lable&Entry)
         fuel=DoubleVar()
-        lable_Fuel=Label(self.newframe, text="Fuel(L)",fg ="#8c8c8c",bg ="White",font=mainFont)     
+        lable_Fuel=Label(self.newframe, text="Fuel(L)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
         lable_Fuel.place(x=0,y=120)
         Entry_Fuel=Entry(self.newframe, textvariable=fuel,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_Fuel.place(x=0,y=140)
         
         #Thrust widgets(Lable&Entry)
         thrust=DoubleVar()
-        lable_Thrust=Label(self.newframe, text="Engine thrust(KN)",fg ="#8c8c8c",bg ="White",font=mainFont)     
+        lable_Thrust=Label(self.newframe, text="Engine thrust(KN)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
         lable_Thrust.place(x=0,y=160)
         Entry_Thrust=Entry(self.newframe, textvariable=thrust,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_Thrust.place(x=0,y=180)
 
         #Burn time widgets(Lable&Entry)
         burnTime=DoubleVar()
-        lable_burnTime=Label(self.newframe, text="Burn Time(S)",fg ="#8c8c8c",bg ="White",font=mainFont)     
+        lable_burnTime=Label(self.newframe, text="Burn Time(S)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
         lable_burnTime.place(x=0,y=200)
         Entry_burnTime=Entry(self.newframe, textvariable=burnTime,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_burnTime.place(x=0,y=220)    
         #Color widgets(canvas,Lable&Entry)
         stageColor=StringVar()
         stageColor.set("#")
-        lable_stageColor=Label(self.newframe, text="Stage color (Hex)",fg ="#8c8c8c",bg ="White",font=mainFont)     
+        lable_stageColor=Label(self.newframe, text="Stage color (Hex)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
         lable_stageColor.place(x=0,y=240)
         Entry_stageColor=Entry(self.newframe, textvariable=stageColor,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_stageColor.place(x=0,y=260)
@@ -198,31 +199,31 @@ class stageFrame:
         colorBox.place(x=100,y=260)
 
         #Adds a stage to the rocket
-        addStageButton=Button(self.newframe,text="+ Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=mainFont)
+        addStageButton=Button(self.newframe,text="+ Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
         addStageButton.place(x=(width/2)-100,y=20)
         addStageButton.bind("<Button-1>",lambda event:changeStageState(event,"Add"))
         #Save the values for the current stage
-        saveStageButton=Button(self.newframe,text="Save Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=mainFont)
+        saveStageButton=Button(self.newframe,text="Save Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
         saveStageButton.place(x=240,y=260)
         saveStageButton.bind("<Button-1>",lambda event:saveStage(event))
         
         #Cycles to the stage before the current one current stage=0 then don't show
-        cycleStageLeft=Button(self.newframe,text="<",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=mainFont)
+        cycleStageLeft=Button(self.newframe,text="<",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
         cycleStageLeft.place(x=(width/4)-20,y=20)
         cycleStageLeft.bind("<Button-1>",lambda event:changeStageState(event,"Left"))
 
         #Cycles to the stage after the current one#current stage=max then don't show
-        cycleStageRight=Button(self.newframe,text=">",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=mainFont)
+        cycleStageRight=Button(self.newframe,text=">",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
         cycleStageRight.place(x=(width/4)+60,y=20)
         cycleStageRight.bind("<Button-1>",lambda event:changeStageState(event,"Right"))
 
-        lable_graphOptions=Label(self.newframe, text="Graph config",fg ="#737373",bg ="White",font=mainFont)
+        lable_graphOptions=Label(self.newframe, text="Graph config",fg ="#737373",bg ="White",font=self.mainFont)
         lable_graphOptions.place(x=0,y=330)
         #Graph type lable
-        lable_graphType=Label(self.newframe, text="Graph type",fg ="#8c8c8c",bg ="White",font=mainFont)
+        lable_graphType=Label(self.newframe, text="Graph type",fg ="#8c8c8c",bg ="White",font=self.mainFont)
         lable_graphType.place(x=0,y=350)
         #Save graph button
-        launchButton=Button(self.newframe,text="Launch",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=mainFont)
+        launchButton=Button(self.newframe,text="Launch",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
         launchButton.place(x=0,y=480)
         launchButton.bind("<Button-1>",lambda event:changeFrame(event,"Graph",sideNumber))#Bind to the function "launch"
 
@@ -244,28 +245,57 @@ class stageFrame:
         
 #GraphFrame        
 class graphFrame:
+    style.use('ggplot')
     newFrame=None
     canvas=None
     figure=None
     x=None
-    xplot=[None,None,None]
-    yplot=[1,2,3]
+    vel=10 #temp
+    xplot=0
+    yplot=0
+
+    def getPlots(self,angle,velocity):
+        xValues=list()
+        yValues=list()
+        gravity=9.81
+        #Resolving the vector into v/h components 
+        verticalComponent=velocity*(sin(radians(angle)))
+        horizontalComponent=velocity*(cos(radians(angle)))
+        #Finds Time/max horizontal displacement/ max vertical displacement 
+        time=(2*verticalComponent)/gravity
+        maxHorizDisp=time*horizontalComponent
+        maxVertDisp=(-(verticalComponent**2)/(2*-gravity))
+        for i in range(0,36):
+            updateTime=(time*(i/35))
+            yValues.append((verticalComponent*updateTime)+((-1/2*gravity)*(updateTime*updateTime)))
+            xValues.append(updateTime*horizontalComponent) 
+        return(xValues,yValues)
         
     def __init__(self,sideNumber):
         self.newFrame=Frame(height=height,width=width/2)
         self.newFrame.config(bg ="white")
         if sideNumber==0:
-            self.xplot=[0,1,2]
             x=0
         else:
-            self.xplot=[2,1,0]
             x=width/2
         #Create the frame
         self.newFrame.place(x=x,y=0)
         #Begin graph creation
         figure = plt.figure(figsize=(4,5), dpi=100,frameon=False,tight_layout=True)
-        a=plt.plot(self.xplot,self.yplot)
-        plt.setp(a, color="blue", linewidth=2.0)
+        if sideNumber==0:
+            for i in range(len(r0StagesList)):
+                angle=r0StagesList[i].angle
+                self.xplot,self.yplot= self.getPlots(angle,self.vel)
+                color=r0StagesList[i].stageColor
+                a0=plt.plot(self.xplot,self.yplot)
+                plt.setp(a0, color=color, linewidth=2.0)
+        else:
+               for i in range(len(r1StagesList)):
+                self.xplot,self.yplot= self.getPlots(r1StagesList[i].angle,self.vel)
+                color=r1StagesList[i].stageColor
+                a1=plt.plot(self.xplot,self.yplot)
+                plt.setp(a1, color=color, linewidth=2.0)
+
         plt.ylabel("Y AXIS")
         plt.xlabel("X AXIS")
         canvas = FigureCanvasTkAgg(figure, self.newFrame)
