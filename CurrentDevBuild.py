8b0a58478c792be803c472bff9f338b22e2b457a
Thomas Martin <ThomasMartin98@hotmail.com>
2016-07-12 21:04:26 +0100

    Update CurrentDevBuild.py
    Removed fuel as an option, there was no need for it as I was just using arbitrary numbers

--- CurrentDevBuild.py
+++ CurrentDevBuild.py
@@ -45,7 +45,6 @@ class stageFrame:
             self.mass=0
             self.angle=0
             self.thrust=0
-            self.amountOfFuel=0
             self.stageTime=0
             self.stageColor="#FFFFFF"
     def __init__(self,sideNumber):
@@ -93,7 +92,6 @@ class stageFrame:
                     print("Saving stage%i from input field %i"%(self.r0currentStageNumber,sideNumber))
                     r0StagesList[self.r0currentStageNumber].mass=float(Entry_Mass.get())
                     r0StagesList[self.r0currentStageNumber].angle=float(Entry_Angle.get())
-                    r0StagesList[self.r0currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
                     r0StagesList[self.r0currentStageNumber].thrust=float(Entry_Thrust.get())
                     r0StagesList[self.r0currentStageNumber].stageTime=float(Entry_stageTime.get())
                     r0StagesList[self.r0currentStageNumber].stageColor=str(Entry_stageColor.get())
@@ -101,7 +99,6 @@ class stageFrame:
                     print("Saving stage%i from input field %i"%(self.r1currentStageNumber,sideNumber))
                     r1StagesList[self.r1currentStageNumber].mass=float(Entry_Mass.get())
                     r1StagesList[self.r1currentStageNumber].angle=float(Entry_Angle.get())
-                    r1StagesList[self.r1currentStageNumber].amountOfFuel=float(Entry_Fuel.get())
                     r1StagesList[self.r1currentStageNumber].thrust=float(Entry_Thrust.get())
                     r1StagesList[self.r1currentStageNumber].stageTime=float(Entry_stageTime.get())
                     r1StagesList[self.r1currentStageNumber].stageColor=str(Entry_stageColor.get())
@@ -154,7 +151,6 @@ class stageFrame:
                 currentStage=r1StagesList[self.r1currentStageNumber]
             mass.set(str(currentStage.mass))
             angle.set(str(currentStage.angle))
-            fuel.set(str(currentStage.amountOfFuel))
             thrust.set(str(currentStage.thrust))
             stageTime.set(str(currentStage.stageTime))
             colorBox.config(bg=currentStage.stageColor)
@@ -201,37 +197,37 @@ class stageFrame:
         Entry_Angle=Entry(self.newframe, textvariable=angle,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
         Entry_Angle.place(x=0,y=100)
         
-        #Fuel widgets(Lable&Entry)
-        fuel=DoubleVar()
-        lable_Fuel=Label(self.newframe, text="Fuel(L)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
-        lable_Fuel.place(x=0,y=120)
-        Entry_Fuel=Entry(self.newframe, textvariable=fuel,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
-        Entry_Fuel.place(x=0,y=140)
+        ##Fuel widgets(Lable&Entry)
+        #fuel=DoubleVar()
+        #lable_Fuel=Label(self.newframe, text="Fuel(L)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
+        #lable_Fuel.place(x=0,y=120)
+        #Entry_Fuel=Entry(self.newframe, textvariable=fuel,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
+        #Entry_Fuel.place(x=0,y=140)
         
         #Thrust widgets(Lable&Entry)
         thrust=DoubleVar()
         lable_Thrust=Label(self.newframe, text="Engine thrust(KN)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
-        lable_Thrust.place(x=0,y=160)
+        lable_Thrust.place(x=0,y=120)
         Entry_Thrust=Entry(self.newframe, textvariable=thrust,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
-        Entry_Thrust.place(x=0,y=180)
+        Entry_Thrust.place(x=0,y=140)
 
         #Burn time widgets(Lable&Entry)
         stageTime=DoubleVar()
         lable_stageTime=Label(self.newframe, text="Time until next stage(S)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
-        lable_stageTime.place(x=0,y=200)
+        lable_stageTime.place(x=0,y=160)
         Entry_stageTime=Entry(self.newframe, textvariable=stageTime,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
-        Entry_stageTime.place(x=0,y=220)    
+        Entry_stageTime.place(x=0,y=180)    
 
         #Color widgets(canvas,Lable&Entry)
         stageColor=StringVar()
         stageColor.set("#")
         lable_stageColor=Label(self.newframe, text="Stage color (Hex)",fg ="#8c8c8c",bg ="White",font=self.mainFont)     
-        lable_stageColor.place(x=0,y=240)
+        lable_stageColor.place(x=0,y=200)
         Entry_stageColor=Entry(self.newframe, textvariable=stageColor,fg ="#E24A33 ",bg ="#E5E5E5",relief=FLAT)
-        Entry_stageColor.place(x=0,y=260)
+        Entry_stageColor.place(x=0,y=220)
         Entry_stageColor.bind("<FocusOut>",lambda event:updateColorBox(event))
         colorBox=Canvas(self.newframe, width=25, height=15)
-        colorBox.place(x=100,y=260)
+        colorBox.place(x=100,y=220)
 
         #Adds a stage to the rocket
         addStageButton=Button(self.newframe,text="+ Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
@@ -239,7 +235,7 @@ class stageFrame:
         addStageButton.bind("<Button-1>",lambda event:changeStageState(event,"Add"))
         #Save the values for the current stage
         saveStageButton=Button(self.newframe,text="Save Stage",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5",font=self.mainFont)
-        saveStageButton.place(x=240,y=260)
+        saveStageButton.place(x=240,y=220)
         saveStageButton.bind("<Button-1>",lambda event:saveStage(event))
         
         #Cycles to the stage before the current one current stage=0 then don't show
@@ -252,11 +248,9 @@ class stageFrame:
         cycleStageRight.place(x=(width/4)+60,y=20)
         cycleStageRight.bind("<Button-1>",lambda event:changeStageState(event,"Right"))
 
-        lable_graphOptions=Label(self.newframe, text="Graph type",fg ="#737373",bg ="White",font=self.mainFont)
-        lable_graphOptions.place(x=0,y=330)
         #Graph widgets
         lable_graphType=Label(self.newframe, text="Graph type",fg ="#8c8c8c",bg ="White",font=self.mainFont)
-        lable_graphType.place(x=0,y=350)
+        lable_graphType.place(x=0,y=290)
         #Dropdown box
         currentGraphType=StringVar()
         currentGraphType.set("Displacement-Time") #default value
@@ -303,13 +297,13 @@ class graphFrame:
     totalTime=0
     
 
-    def getPlots(self,totalMass,angle,engineThrust,amountOfFuel,stageTime,graphType):
+    def getPlots(self,totalMass,angle,engineThrust,stageTime,graphType):
         """while """
         xValues=list()
         yValues=list()
         xAxis=""
         yAxis=""
-        g=9.81
+        g=-9.81
 
         #Resolving the engine thrust vector into its v/h components 
         verticalThrust=engineThrust*(sin(radians(angle)))
@@ -317,11 +311,7 @@ class graphFrame:
 
         for i in range(0,36):
             updateTime=(stageTime*(i/35))
-            amountOfFuel-=10
-            if (amountOfFuel<0):
-                verticalThrust=0   
-                horizontalThrust=0           
-                print( "amountOfFuel<0")          
+         
 
             #Net Force
             netVForce=verticalThrust-(totalMass*g)#Upthrust-weight
@@ -402,9 +392,8 @@ class graphFrame:
             #Get all of the values from all of the inputs    
             angle=stagesListUsed[i].angle
             engineThrust=stagesListUsed[i].thrust
-            amountOfFuel=stagesListUsed[i].amountOfFuel
             stageTime=stagesListUsed[i].stageTime
-            self.xplot,self.yplot,xAxisText,yAxisText= self.getPlots(totalMass,angle,engineThrust,amountOfFuel,stageTime,str(graphType))
+            self.xplot,self.yplot,xAxisText,yAxisText= self.getPlots(totalMass,angle,engineThrust,stageTime,str(graphType))
             color=stagesListUsed[i].stageColor
             a0=plt.plot(self.xplot,self.yplot)
             plt.setp(a0, color=color, linewidth=3.0)
