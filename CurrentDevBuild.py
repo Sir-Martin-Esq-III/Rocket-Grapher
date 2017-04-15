c5f97c5f38358075f892e28fc459eaebbb1e6f1a
Thomas Martin <ThomasMartin98@hotmail.com>
2017-04-15 14:23:30 +0100

    Update CurrentDevBuild.py
    Added the ability to delete stages 
    Added the ability to read stages from a csv
    Changed UI again
    Better image saving

--- CurrentDevBuild.py
+++ CurrentDevBuild.py
@@ -1,3 +1,5 @@
+#SORT OUT STAGEFRAME METHODS ISSUE
+import csv
 from tkinter import *
 from math import *
 from tkinter import filedialog   
@@ -7,11 +9,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-
 sideFrames=[None,None]
-r0StagesList=list()#List of the stages for Rocket0
-r1StagesList=list() #List of the stages for Rocket1
-
 
 root = Tk()
 #Gets the height and width of the monitor
@@ -40,19 +38,16 @@ class stageFrame:
         stageColor="#FFFFFF"
         
             
-    def __init__(self,sideNumber):
+    def __init__(self,xPos):
         errors=[0,0]
-        
+        self.x=xPos
         #Checks what rocket this frame is for and does the appropriate variable assignments
-        if sideNumber==0:
-            self.x=0
-            self.tempStageArray=r0StagesList
-            #self.tempStageNumber=self.r0currentStageNumber
-
-        else:
-           self.x=width/2
-           self.tempStageArray=r1StagesList
-           #self.tempStageNumber=self.r1currentStageNumber
+        #if sideNumber==0:
+        #    self.x=0
+        #    self.tempStageArray=r0StagesList
+        #else:
+         #  self.x=width/2
+         #  self.tempStageArray=r1StagesList
 
         if len(self.tempStageArray)==0:
             startStage=self.Stages()
@@ -100,8 +95,7 @@ class stageFrame:
             #Add a stage
             if option=="Add":
                 self.tempStageNumber =len(self.tempStageArray)
-                newStage=self.Stages()
-                self.tempStageArray.append(newStage)
+                self.tempStageArray.append(self.Stages())
             #Cycle left
             elif option=="Left":
                 if self.tempStageNumber!=0:
@@ -110,7 +104,14 @@ class stageFrame:
             elif option=="Right":
                 if self.tempStageNumber<len(self.tempStageArray)-1:
                     self.tempStageNumber+=1
-
+            #Remove a stage
+            elif option=="Del":
+                del self.tempStageArray[self.tempStageNumber]
+                if len(self.tempStageArray)==0:#If there are no more stages left                 
+                    self.tempStageArray.append(self.Stages())
+                else:
+                    if self.tempStageNumber!=0:
+                        self.tempStageNumber-=1       
             #Sets all of the entry widgets to the value of the currentStages class instance 
             stageValue.set("Stage %i"%(self.tempStageNumber))
             currentStage=self.tempStageArray[self.tempStageNumber]
@@ -120,7 +121,33 @@ class stageFrame:
             stageTime.set(str(currentStage.stageTime))
             colorBox.config(bg=currentStage.stageColor)
             stageColor.set(str(currentStage.stageColor))
-            
+
+        def readFile(event):
+            self.tempStageNumber=0
+            del self.tempStageArray[:]
+            #new
+            self.tempStageArray.append(self.Stages())
+            lineCounter=0
+            amountOfLines=0
+            fileName=input("What is the file name: ")
+            try:
+                with open(fileName+'.csv') as csvFile:
+                    readCSV = csv.reader(csvFile,delimiter=',')
+                    amountOfLines=len(open("test.csv").readlines())           
+                    for row in readCSV:
+                        mass.set(str(row[0]))
+                        angle.set(str(row[1]))
+                        thrust.set(str(row[2]))
+                        stageTime.set(str(row[3]))
+                        stageColor.set(str(row[4]))
+                        saveStage(None)
+                        if lineCounter!=amountOfLines-1:#If this is not the last line in the file
+                            changeStageState(None,"Add")
+                        lineCounter+=1
+                    csvFile.close()
+            except FileNotFoundError:
+                print("The file cannot be found. Please make sure you input it correctly")
+
         #Create a new frame
         self.newframe=Frame(height=height,width=width/2)
         self.newframe.config(bg ="#dbdbdb")
@@ -129,7 +156,7 @@ class stageFrame:
         self.newframe.place(x=self.x,y=0)
         
         """---BEGIN UI PLACEMENT---"""
-        lable_rocketValue=Label(self.newframe,text="Rocket "+str(sideNumber),fg ="#039be5",bg ="#dbdbdb",font=self.titleFont)
+        lable_rocketValue=Label(self.newframe,text="Rocket "+str(int(self.x>0)),fg ="#039be5",bg ="#dbdbdb",font=self.titleFont)
         lable_rocketValue.place(x=width/4,y=0)
 
         #Error text
@@ -144,6 +171,10 @@ class stageFrame:
         lable_stageValue=Label(self.newframe, textvariable=stageValue,fg ="#29b6f6",bg ="#dbdbdb",font=self.mainFont)
         lable_stageValue.place(x=width/4,y=20)
         
+        #Rocket parameters lable
+        lable_RParam=Label(self.newframe, text="Rocket Parameters: ",fg ="#039be5",bg ="#dbdbdb",font=self.mainFont)
+        lable_RParam.place(x=0,y=20)
+
         #Mass widgets(Lable&Entry)
         mass=DoubleVar()
         lable_Mass=Label(self.newframe, text="Mass(KG)",fg ="#29b6f6",bg ="#dbdbdb",font=self.mainFont)   
@@ -167,7 +198,7 @@ class stageFrame:
 
         #Burn time widgets(Lable&Entry)
         stageTime=DoubleVar()
-        lable_stageTime=Label(self.newframe, text="Time until next stage(S)",fg ="#29b6f6",bg ="#dbdbdb",font=self.mainFont)     
+        lable_stageTime=Label(self.newframe, text="Burn time(S)",fg ="#29b6f6",bg ="#dbdbdb",font=self.mainFont)     
         lable_stageTime.place(x=0,y=160)
         Entry_stageTime=Entry(self.newframe, textvariable=stageTime,fg ="#039be5 ",bg ="#c9c9c9",relief=FLAT)
         Entry_stageTime.place(x=0,y=180)    
@@ -185,12 +216,12 @@ class stageFrame:
     
 
         #Adds a stage to the rocket
-        addStageButton=Button(self.newframe,text="+ Stage",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
-        addStageButton.place(x=(width/2)-100,y=20)
+        addStageButton=Button(self.newframe,text="Add stage",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
+        addStageButton.place(x=0,y=320)
         addStageButton.bind("<Button-1>",lambda event:changeStageState(event,"Add"))
         #Save the values for the current stage
         saveStageButton=Button(self.newframe,text="Save Stage",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
-        saveStageButton.place(x=240,y=220)
+        saveStageButton.place(x=0,y=400)
         saveStageButton.bind("<Button-1>",lambda event:saveStage(event))
         
         #Cycles to the stage before the current one current stage=0 then don't show
@@ -203,9 +234,22 @@ class stageFrame:
         cycleStageRight.place(x=(width/4)+60,y=20)
         cycleStageRight.bind("<Button-1>",lambda event:changeStageState(event,"Right"))
 
+        #Cycles to the stage after the current one#current stage=max then don't show
+        deleteCurrentStage=Button(self.newframe,text="Delete current stage",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
+        deleteCurrentStage.place(x=0,y=360)
+        deleteCurrentStage.bind("<Button-1>",lambda event:changeStageState(event,"Del"))
+        
+        #Adds a stage to the rocket
+        ReadFileButton=Button(self.newframe,text="Read file",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
+        ReadFileButton.place(x=0,y=440)
+        ReadFileButton.bind("<Button-1>",lambda event:readFile(event))
+
+        lable_StageOptions=Label(self.newframe, text="Stage options:",fg ="#039be5",bg ="#dbdbdb",font=self.mainFont)
+        lable_StageOptions.place(x=0,y=300)
+
         #Graph widgets
-        lable_graphType=Label(self.newframe, text="Graph type",fg ="#29b6f6",bg ="#dbdbdb",font=self.mainFont)
-        lable_graphType.place(x=0,y=290)
+        lable_graphType=Label(self.newframe, text="Graph type: ",fg ="#039be5",bg ="#dbdbdb",font=self.mainFont)
+        lable_graphType.place(x=0,y=250)
         #Dropdown box
         currentGraphType=StringVar()
         currentGraphType.set("Displacement-Time") #default value
@@ -214,18 +258,18 @@ class stageFrame:
                     "XDisplacement-YDisplacement",
                     "XVelocity-YVelocity"]
         w =  OptionMenu(self.newframe, currentGraphType , *graphTypes)
-        w.place(x=0,y=350)
+        w.place(x=0,y=270)
         w.config(bg="#c9c9c9",relief=FLAT,highlightthickness=0,activebackground="#afafaf")
 
         #Launch button
         launchButton=Button(self.newframe,text="Launch",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9",font=self.mainFont)
-        launchButton.place(x=0,y=height-50)
+        launchButton.place(x=0,y=height-launchButton.winfo_reqheight())
         launchButton.bind("<Button-1>",lambda event:launch(event))#Bind to the function "launch"
 
         def launch(event):
             if checkErrors()==False:
                 graphType=str(currentGraphType.get())
-                changeFrame(event,"Graph",sideNumber,graphType)
+                changeFrame(event,"Graph",int(self.x>0),graphType,self.tempStageArray)
             
         #This is so when we graph->input it loads current stage
         changeStageState(None,"None")
@@ -257,7 +301,7 @@ class graphFrame:
        
         fy=fx=vy=vx=py=px=t=0
         g=-9.81
-        dt=0.01
+        dt=0.1
         thrust= stageList[0].thrust
 
         while py>=0:
@@ -285,26 +329,27 @@ class graphFrame:
             vx+=(fx/totalMass)*dt
             py+=vy*dt
             px+=vx*dt   
+            print("Vy: ",vy," Py: ",py)
                
-            if graphType=="('Displacement-Time',)":
+            if graphType=="Displacement-Time":
                 xValues.append(dt+t)              
                 yValues.append(py)
                 xAxis="Time (s)"
                 yAxis="Vertical displacement (m)"
 
-            elif graphType=="('Velocity-Time',)":
+            elif graphType=="Velocity-Time":
                 yValues.append(vy)
                 xValues.append(dt+t)
                 xAxis="Time (s)"
                 yAxis="Vertical velocity (m/s^-1)"
                 
-            elif graphType=="('XDisplacement-YDisplacement',)":
+            elif graphType=="XDisplacement-YDisplacement":
                 yValues.append(py)
                 xValues.append(px)
                 xAxis="Horizontal displacement (m)"
                 yAxis="Vertical displacement (m)"
                 
-            elif graphType=="('XVelocity-YVelocity',)":
+            elif graphType=="XVelocity-YVelocity":
                 yValues.append(vy)
                 xValues.append(vx)
                 xAxis="Horizontal velocity (m/s^-1)"
@@ -320,20 +365,17 @@ class graphFrame:
         canvas.get_tk_widget().place(x=0,y=0)
          
         
-    def __init__(self,sideNumber,graphType):
+    def __init__(self,xPos,graphType,stagesList):
         xAxisText=""
         yAxisText=""
+        print(graphType)
         self.newFrame=Frame(height=height,width=width/2)
         self.newFrame.config(bg ="white")
         sepTimeQueue=[]
         totalStageTime=0
-  
-        if sideNumber==0:
-            self.x=0
-            self.currentRocketStages=r0StagesList
-        else:
-            self.currentRocketStages=r1StagesList
-            self.x=width/2
+        self.x=xPos
+        self.currentRocketStages=stagesList
+     
         #Create the frame
         self.newFrame.place(x=self.x,y=0)
         widthInches= root.winfo_screenwidth() / root.winfo_fpixels('1i')
@@ -351,18 +393,26 @@ class graphFrame:
         stagesListCopy=list(self.currentRocketStages)
         self.getPlots(str(graphType),sepTimeQueue,stagesListCopy)
  
-        returnButton=Button(self.newFrame,text="Return to input",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5")
-        returnButton.place(x=(width/2)-125,y=0)
-        returnButton.bind("<1>",lambda event:changeFrame(event,"Input",sideNumber))
+        returnButton=Button(self.newFrame,text="Return",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9")
+        returnButton.place(x=(width/2)-returnButton.winfo_reqwidth(),y=0)
+        returnButton.bind("<1>",lambda event:changeFrame(event,"Input",int(self.x>0),None,None))
 
-        saveButton=Button(self.newFrame,text="Save",fg ="#E24A33 ",relief=FLAT,bg="#E5E5E5")
-        saveButton.place(x=(width/2)-400,y=0)
+        saveButton=Button(self.newFrame,text="Save",fg ="#039be5 ",relief=FLAT,bg="#c9c9c9")
+        saveButton.place(x=0,y=0)
         #Will need to choose a more suitable DIR for the picture
-        saveButton.bind("<1>",plt.savefig(r"C:\Users\Thoma\Pictures\Test images\Image.png"))
+        saveButton.bind("<1>",lambda event:self.saveImage(event))
+
+
+    def saveImage(self,event):
+        dir=input("Please enter the directory you want to save this image ")
+        #dir.append("\\")
+        imageName=input("What do you want this to be called? ")
+        plt.savefig((dir+ "\\" +imageName))
+    
+
         
     #Destroys the frame
     def destroy(self,sideNumber):
-        sideFrames[sideNumber].newFrame.destroy()
         sideFrames[sideNumber].newFrame.destroy()
        
 """ChangeFrame(Event: tkEvent-- Has to be passed when the user inputs something from a .bind function
@@ -370,12 +420,15 @@ class graphFrame:
                SideNumber: Integer--0=left, 1=right)  
 Is called when the frame needs to be changed to show a graph/input field
 """
-def changeFrame(event,Type,sideNumber,*args):
+def changeFrame(event,Type,sideNumber,graphtype,stageslist):
+    xPos=0
     Destroyer(event, sideNumber)
+    if sideNumber!=0:
+        xPos=width/2
     if Type=="Input":
-        sideFrames[sideNumber]=stageFrame(sideNumber)
+        sideFrames[sideNumber]=stageFrame(xPos)
     else:
-        sideFrames[sideNumber]=graphFrame(sideNumber,args)
+        sideFrames[sideNumber]=graphFrame(xPos,graphtype, stageslist)
 
 """Destroyer(Event: tkEvent-- Has to be passed when the user inputs something from a .bind function
             SideNumber: Integer--0=left, 1=right)  
@@ -393,8 +446,8 @@ def Destroyer(event,sideNumber):
 This is called to show input fields when the program runs.
 """
 def init():
-    changeFrame(None,"Input",0)
-    changeFrame(None,"Input",1)
+    changeFrame(None,"Input",0,None,None)
+    changeFrame(None,"Input",1,None,None)
 
 init()
 root.mainloop()
